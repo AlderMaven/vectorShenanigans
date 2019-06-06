85bc552e9711698dc14c3db83b608917e8754887
Alder Maven <43149194+AlderMaven@users.noreply.github.com>
2019-06-06 16:58:44 -0500

    Changed comparitor, added functions
    Changed comparitor to equals() for ease of use and changed to monoarguement for simplicity, added isGreater and isLesser comparitors, comments added

--- vectorStuff.cpp
+++ vectorStuff.cpp
@@ -13,27 +13,86 @@ class intVectorShenanigans{
 	vector<int> containedData;
 	
 	public:
+	//Returns size of containedData
 	int size(){
 		return this->containedData.size();
 	}
 	
+	//returns value from containedData[position]
 	int getValue(int position){
 		return this->containedData[position];
 	}
 	
-	bool comparator(intVectorShenanigans first, intVectorShenanigans second){
-		if(first.size() != second.size()){
+	//Returns true if sum of vector containedData is == in source and in arguement
+	bool equals(intVectorShenanigans second){
+		if(this->containedData.size() != second.size()){
 			return false;
 		}
-		int temp = first.size();
+		int temp = this->size();
 		for(int i=0; i<temp; i++){
-			if(first.getValue(i) != second.getValue(i)){
+			if(this->getValue(i) != second.getValue(i)){
 				return false;
 			}
 		}
 		return true;
 	}
 	
+	//Returns true if sum of vector containedData is > in source than in arguement
+	bool isGreater(intVectorShenanigans second){
+		int accumulatorOne;
+		int accumulatorTwo;
+		int temp = 0;
+		int tempB;
+		while(temp<this->size() && temp<second.size()){
+			accumulatorOne += this->getValue(temp);
+			accumulatorTwo += second.getValue(temp);
+			temp++;
+		}
+		tempB = temp;
+		while(temp<this->size()){
+			accumulatorOne += this->getValue(temp);
+			temp++;
+		}
+		while(tempB<second.size()){
+			accumulatorTwo += second.getValue(tempB);
+			tempB++;
+		}
+		if(accumulatorOne>accumulatorTwo){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	//Returns true if sum of vector containedData is < in source than in arguement
+	bool isLesser(intVectorShenanigans second){
+		int accumulatorOne;
+		int accumulatorTwo;
+		int temp = 0;
+		int tempB;
+		while(temp<this->size() && temp<second.size()){
+			accumulatorOne += this->getValue(temp);
+			accumulatorTwo += second.getValue(temp);
+			temp++;
+		}
+		tempB = temp;
+		while(temp<this->size()){
+			accumulatorOne += this->getValue(temp);
+			temp++;
+		}
+		while(tempB<second.size()){
+			accumulatorTwo += second.getValue(tempB);
+			tempB++;
+		}
+		if(accumulatorOne<accumulatorTwo){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	//inserts toAdd into containedData using push_back
 	void addInt(int toAdd){
 		this->containedData.push_back(toAdd);
 		return;
@@ -49,10 +108,41 @@ class intVectorShenanigans{
 	
 };
 
+//testbed
 int main(){
 	
 	vector<int> tempOne;
 	vector<int> tempTwo;
+	
+	for(int i = 1; i<11; i++){
+		tempOne.push_back(i);
+	}
+	for(int i = 1; i<10; i++){
+		tempTwo.push_back(i);
+	}
+	intVectorShenanigans A = intVectorShenanigans(tempOne);
+	intVectorShenanigans B = intVectorShenanigans(tempTwo);
+	if(A.equals(B)){
+		cout << "is equal" << endl;
+	}
+	else{
+		cout << "is not equal" << endl;
+	}
+	if(A.isGreater(B)){
+		cout << "is Greater" << endl;
+	}
+	else{
+		cout << "is not Greater" << endl;
+	}
+	if(A.isLesser(B)){
+		cout << "is Lesser" << endl;
+	}
+	else{
+		cout << "is not Lesser" << endl;
+	}
+
+	
+	
 	
 	return 0;
 }
